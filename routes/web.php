dfc2b196d81ad9184b2aee87f1824bb668ff2dae
3A532017 <s3a532017@student.ncut.edu.tw>
2018-10-22 16:28:01 +0800

    建構佈局與tasks視圖(View)

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+//顯示我們所有任務的清單
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+    //
+});
+
+// 刪除任務
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
