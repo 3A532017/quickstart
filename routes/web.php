f92ada17cb4c160576b561c6870c969c2842d894
3A532017 <s3a532017@student.ncut.edu.tw>
2018-10-22 16:39:41 +0800

    增加任務

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,18 @@ Route::get('/', function () {
 
 // 增加新的任務
 Route::post('/task', function (Request $request) {
+
     // 驗證輸入
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
+    $validator = Validator::make( $request->all(), [
+        'name' => 'required|max:255',      //驗證規則
     ]);
-    if ($validator->fails()) {
+
+    if ($validator->fails()) {      //驗證失敗處理
         return redirect('/')
             ->withInput()
             ->withErrors($validator);
     }
+
     // 建立該任務...
     $task = new Task;
     $task->name = $request->name;
